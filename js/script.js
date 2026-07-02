61b0f9969b0f9cd5f4e454c36f6aa972c453eb96
Ana Crown <acorona1011@gmail.com>
2026-07-02 11:07:54 -0600

    Transición en css y js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => { /*Espera a que la página termine de cargar.*/
+
+    document.querySelector(".hero").classList.add("show"); /*Muestra el hero cuando terminó de cargar la pagina con la trancisión*/
+
+});
